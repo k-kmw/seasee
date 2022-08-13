acb6ef9a5e174858757a7cb71bfb43679ffb8eae
k-kmw <kimmw2_@naver.com>
2022-08-14 01:39:15 +0900

    people

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
+app.use(express.static(path.join(__dirname, '/public')))
 app.listen(3000, () => {
     console.log("3000");
 })
@@ -11,3 +11,157 @@ app.listen(3000, () => {
 app.get('/', (req, res)=>{
     res.render('home');
 })
+
+app.get('/weather', function(req,res){
+    var request = require('request');
+    var url = 'http://apis.data.go.kr/1360000/BeachInfoservice/getUltraSrtFcstBeach?serviceKey=Ttlq1ffL8kVaF2WzhW13DkIZRhzmTLgKbmHgakqODvcbe%2FC9ZRC0klrwGUbssZ1cBRs1NxiRscuJDqul0ukKhg%3D%3D&dataType=JSON'
+    var beach_code = req.query.beach_code;
+    var now = new Date();
+    var Y = now.getFullYear();
+    var M = now.getMonth()+1;
+    var D = now.getDate();
+    var H = now.getHours();
+  
+    if(M<10){
+      M = '0'+String(M)
+    }
+  
+    if(D<10){
+      D = '0'+String(D)
+    }
+  
+    if(H == 0){
+      H='2330'
+      var yesterDate = now.getTime() - (1 * 24 * 60 * 60 * 1000);
+      now.setTime(yesterDate);
+      
+      Y = now.getFullYear();
+      M = now.getMonth() + 1;
+      D = now.getDate();
+          
+      if(M < 10){ 
+        M = "0" + String(M); 
+      }
+      if(D < 10) { 
+        D = "0" + String(D);
+      }
+    }
+    else{
+      if(H-1 < 10){
+        H = "0" + String(H-1);
+      }
+      H = String(H-1) + "30";
+    }
+  
+    var ymd = Y + "" + M + "" + D;
+    var hours = H;
+  
+    url = url + "&base_date=" + ymd + "&base_time=" + hours + "&beach_num=" + beach_code + "&numOfRows=24";
+    
+    var options = {
+      'method': 'GET',
+      'url': url,
+      'headers': {
+      }
+    };
+    
+    var time = [];
+    var pty = [];
+    var sky = [];
+  
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      result = JSON.parse(response.body).response.body.items;
+  
+      for (let index = 0; index < 24; index++) {
+        switch(result.item[index].category){
+          case "PTY":
+            pty.push(result.item[index].fcstValue);
+            break;
+          case "SKY":
+            time.push(result.item[index].fcstTime)
+            switch(result.item[index].fcstValue){
+              case "1":
+                sky.push("맑음");
+                break;
+              case "2":
+                sky.push("구름조금");
+                break;
+              case "3":
+                sky.push("구름많음");
+                break;
+              case "4":
+                sky.push("흐림");
+                break;
+            }
+            break;
+          default:
+            break;
+        }
+      };
+  
+      for (let index = 0; index < sky.length; index++) {
+        if (pty[index] == "1" || pty[index] == "4" || pty[index] == "5" || pty[index] == "6") {
+          sky[index] = "비";
+        }
+        else if(pty[index] != "0"){
+          sky[index] = "눈";
+        }
+      }
+  
+      var data = new Object();
+      var list = new Array();
+  
+      for (let index = 0; index < 6; index++) {
+          var testList = new Array();
+          testList.push(time[index]);
+          testList.push(sky[index]);
+          list.push(testList);
+      }
+      data.weather = list;
+  
+      var jsonData = JSON.stringify(data);
+  
+      res.send(jsonData)
+    })
+  })
+  
+  app.get('/info', function(req,res) { 
+    var request = require('request');
+    var url2 = 'https://www.tournmaster.com/seantour_map/travel/getBeachCongestionApi.do';
+    
+    var beach_name = encodeURI(req.query.beach_name);
+  
+    var options = {
+      'method': 'GET',
+      'url': url2,
+      'headers': {
+      }
+    };
+  
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+  
+      var data = JSON.parse(response.body)
+      
+      var current_people = -1;
+      var congestion = -1;
+  
+      for (key in data) {
+        if (beach_name == encodeURI(data[key].poiNm)) {
+          current_people = data[key].uniqPop
+          congestion = data[key].congestion
+          break;
+        }
+      }
+  
+      var data = new Object() ;
+          
+      data.current_people = current_people;
+      data.congestion = congestion;
+    
+      var jsonData = JSON.stringify(data);
+  
+      res.send(jsonData)
+    });
+  })
---
+++ public/app.js
@@ -0,0 +1,127 @@
+// var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+//     mapOption = { 
+//         center: new kakao.maps.LatLng(35.450701, 127.570667), // 지도의 중심좌표
+//         level: 13 // 지도의 확대 레벨
+//     };
+
+// var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// const markers = [[35.15833, 129.15992, "해운대해수욕장"], [35.15300, 129.11896, "광안리해수욕장"]]
+
+// markers.forEach(m => {
+//     // 마커가 표시될 위치입니다
+//     var markerPosition  = new kakao.maps.LatLng(m[0], m[1]);
+//     // 마커를 생성합니다 
+//     var marker = new kakao.maps.Marker({
+//         position: markerPosition,
+//         clickable: true,
+//         text: m[2]
+//     });
+//     var iwContent = `<div style="padding:5px;">${m[2]} <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+//     iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+
+//     // 인포윈도우를 생성합니다
+//     var infowindow = new kakao.maps.InfoWindow({
+//         position : iwPosition, 
+//         content : iwContent 
+//     });
+  
+// // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+// infowindow.open(map, marker); 
+//     // 마커가 지도 위에 표시되도록 설정합니다
+//     marker.setMap(map);
+// })
+
+// 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };  
+
+// 지도를 생성합니다    
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places(); 
+
+// 키워드로 장소를 검색합니다
+ps.keywordSearch('해수욕장 ', placesSearchCB); 
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }       
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
+}
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x),
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+        const placeName = place.place_name;
+        const index = placeName.indexOf('해');
+        let name = placeName.slice(0, index);
+        if(placeName === '해운대해수욕장') {
+            name = '해운대';
+        }
+        getRes(name)
+        .then(res => {
+            const people = res.data.current_people;
+            const deleting = document.querySelector('span');
+            if(deleting !== null) {
+                deleting.remove();
+            }
+            const span = document.createElement('span');
+            if(people === -1) {
+                span.append('정보 없음')
+            }
+            else {
+            span.append(`현재 사람수: ${res.data.current_people}`);
+            }
+            document.body.append(span);
+        })
+        
+
+    });
+}
+
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+    
+    // 클릭한 위도, 경도 정보를 가져옵니다 
+    var latlng = mouseEvent.latLng;
+    
+    var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+    
+    var resultDiv = document.getElementById('result'); 
+    resultDiv.innerHTML = message;
+    
+});
+
+async function getRes(loc) {
+    return await axios.get(`http://localhost:3000/info?beach_name=${loc}`)
+}
