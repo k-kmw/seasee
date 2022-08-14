a6afe0e310f881e550cb455ec60dd7fc43031d68
Hyun-Jiii <hyunjiiiii.0105@gmail.com>
2022-08-14 09:01:35 +0900

    merge

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static("views"));
 app.listen(3000, () => {
+  console.log("3000");
+});
 
-    console.log("3000");
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/', (req, res)=>{
-    res.render('home');
-})
+app.use(express.static("public"));
 
-app.use(express.static('public'))
-
-app.get('/weather', function(req,res){
-  var request = require('request');
+app.get("/weather", function (req, res) {
+  var request = require("request");
 
   // 위도경도 -> xy좌표
   var RE = 6371.00877; // 지구 반경(km)
@@ -30,122 +29,117 @@ app.get('/weather', function(req,res){
   var YO = 136; // 기1준점 Y좌표(GRID)
 
   function dfs_xy_conv(code, v1, v2) {
+    var DEGRAD = Math.PI / 180.0;
+    var RADDEG = 180.0 / Math.PI;
 
- 
-      var DEGRAD = Math.PI / 180.0;
-      var RADDEG = 180.0 / Math.PI;
+    var re = RE / GRID;
+    var slat1 = SLAT1 * DEGRAD;
+    var slat2 = SLAT2 * DEGRAD;
+    var olon = OLON * DEGRAD;
+    var olat = OLAT * DEGRAD;
 
-      var re = RE / GRID;
-      var slat1 = SLAT1 * DEGRAD;
-      var slat2 = SLAT2 * DEGRAD;
-      var olon = OLON * DEGRAD;
-      var olat = OLAT * DEGRAD;
+    var sn =
+      Math.tan(Math.PI * 0.25 + slat2 * 0.5) /
+      Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+    var sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sf = (Math.pow(sf, sn) * Math.cos(slat1)) / sn;
+    var ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+    ro = (re * sf) / Math.pow(ro, sn);
+    var rs = {};
+    if (code == "toXY") {
+      rs["lat"] = v1;
+      rs["lng"] = v2;
+      var ra = Math.tan(Math.PI * 0.25 + v1 * DEGRAD * 0.5);
+      ra = (re * sf) / Math.pow(ra, sn);
+      var theta = v2 * DEGRAD - olon;
+      if (theta > Math.PI) theta -= 2.0 * Math.PI;
+      if (theta < -Math.PI) theta += 2.0 * Math.PI;
+      theta *= sn;
+      rs["x"] = Math.floor(ra * Math.sin(theta) + XO + 0.5);
+      rs["y"] = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+    } else {
+      rs["x"] = v1;
+      rs["y"] = v2;
+      var xn = v1 - XO;
+      var yn = ro - v2 + YO;
+      ra = Math.sqrt(xn * xn + yn * yn);
+      if (sn < 0.0) -ra;
+      var alat = Math.pow((re * sf) / ra, 1.0 / sn);
+      alat = 2.0 * Math.atan(alat) - Math.PI * 0.5;
 
-      var sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5);
-      sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
-      var sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
-      sf = Math.pow(sf, sn) * Math.cos(slat1) / sn;
-      var ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
-      ro = re * sf / Math.pow(ro, sn);
-      var rs = {};
-      if (code == "toXY") {
-          rs['lat'] = v1;
-          rs['lng'] = v2;
-          var ra = Math.tan(Math.PI * 0.25 + (v1) * DEGRAD * 0.5);
-          ra = re * sf / Math.pow(ra, sn);
-          var theta = v2 * DEGRAD - olon;
-          if (theta > Math.PI) theta -= 2.0 * Math.PI;
-          if (theta < -Math.PI) theta += 2.0 * Math.PI;
-          theta *= sn;
-          rs['x'] = Math.floor(ra * Math.sin(theta) + XO + 0.5);
-          rs['y'] = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+      if (Math.abs(xn) <= 0.0) {
+        theta = 0.0;
+      } else {
+        if (Math.abs(yn) <= 0.0) {
+          theta = Math.PI * 0.5;
+          if (xn < 0.0) -theta;
+        } else theta = Math.atan2(xn, yn);
       }
-      else {
-          rs['x'] = v1;
-          rs['y'] = v2;
-          var xn = v1 - XO;
-          var yn = ro - v2 + YO;
-          ra = Math.sqrt(xn * xn + yn * yn);
-          if (sn < 0.0) - ra;
-          var alat = Math.pow((re * sf / ra), (1.0 / sn));
-          alat = 2.0 * Math.atan(alat) - Math.PI * 0.5;
-
-          if (Math.abs(xn) <= 0.0) {
-              theta = 0.0;
-          }
-          else {
-              if (Math.abs(yn) <= 0.0) {
-                  theta = Math.PI * 0.5;
-                  if (xn < 0.0) - theta;
-              }
-              else theta = Math.atan2(xn, yn);
-          }
-          var alon = theta / sn + olon;
-          rs['lat'] = alat * RADDEG;
-          rs['lng'] = alon * RADDEG;
-      }
-      return rs;
+      var alon = theta / sn + olon;
+      rs["lat"] = alat * RADDEG;
+      rs["lng"] = alon * RADDEG;
+    }
+    return rs;
   }
 
-  var url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?'
-  + 'serviceKey=%2B6ikhw3jlfLZuDjMkg0XUe9e3NzjWBeTwDrGyTB5BQLe3yZ5xIS5h%2BsQ6qTbyRrzvkPfZh4f%2B0Fc1x1a5t4FBg%3D%3D'
-  + '&dataType=JSON'
+  var url =
+    "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?" +
+    "serviceKey=%2B6ikhw3jlfLZuDjMkg0XUe9e3NzjWBeTwDrGyTB5BQLe3yZ5xIS5h%2BsQ6qTbyRrzvkPfZh4f%2B0Fc1x1a5t4FBg%3D%3D" +
+    "&dataType=JSON";
   var lat = parseFloat(req.query.lat);
   var lng = parseFloat(req.query.lng);
-  var nx = dfs_xy_conv('toXY', lat, lng).x
-  var ny = dfs_xy_conv('toXY', lat, lng).y
+  var nx = dfs_xy_conv("toXY", lat, lng).x;
+  var ny = dfs_xy_conv("toXY", lat, lng).y;
 
-  url = url + '&nx=' + nx + '&ny=' + ny;
-  
+  url = url + "&nx=" + nx + "&ny=" + ny;
+
   var now = new Date();
   var Y = now.getFullYear();
-  var M = now.getMonth()+1;
+  var M = now.getMonth() + 1;
   var D = now.getDate();
   var H = now.getHours();
 
-  if(M<10){
-    M = '0'+String(M)
+  if (M < 10) {
+    M = "0" + String(M);
   }
 
-  if(D<10){
-    D = '0'+String(D)
+  if (D < 10) {
+    D = "0" + String(D);
   }
 
-  if(H == 0){
-    H='2330'
-    var yesterDate = now.getTime() - (1 * 24 * 60 * 60 * 1000);
+  if (H == 0) {
+    H = "2330";
+    var yesterDate = now.getTime() - 1 * 24 * 60 * 60 * 1000;
     now.setTime(yesterDate);
-    
+
     Y = now.getFullYear();
     M = now.getMonth() + 1;
     D = now.getDate();
-        
-    if(M < 10){ 
-      M = "0" + String(M); 
+
+    if (M < 10) {
+      M = "0" + String(M);
     }
-    if(D < 10) { 
+    if (D < 10) {
       D = "0" + String(D);
     }
-  }
-  else{
-    if(H-1 < 10){
-      H = "0" + String(H-1) + "30";
-    }
-    else H = String(H-1) + "30";
+  } else {
+    if (H - 1 < 10) {
+      H = "0" + String(H - 1) + "30";
+    } else H = String(H - 1) + "30";
   }
 
   var ymd = Y + "" + M + "" + D;
   var hours = H;
 
   url = url + "&base_date=" + ymd + "&base_time=" + hours + "&numOfRows=24";
-  
+
   var options = {
-    'method': 'GET',
-    'url': url,
-    'headers': {
-    }
+    method: "GET",
+    url: url,
+    headers: {},
   };
-  
+
   var time = [];
   var pty = [];
   var sky = [];
@@ -155,13 +149,13 @@ app.get('/weather', function(req,res){
     result = JSON.parse(response.body).response.body.items;
 
     for (let index = 0; index < 24; index++) {
-      switch(result.item[index].category){
+      switch (result.item[index].category) {
         case "PTY":
           pty.push(result.item[index].fcstValue);
           break;
         case "SKY":
-          time.push(result.item[index].fcstTime)
-          switch(result.item[index].fcstValue){
+          time.push(result.item[index].fcstTime);
+          switch (result.item[index].fcstValue) {
             case "1":
               sky.push("맑음");
               break;
@@ -179,13 +173,17 @@ app.get('/weather', function(req,res){
         default:
           break;
       }
-    };
+    }
 
     for (let index = 0; index < sky.length; index++) {
-      if (pty[index] == "1" || pty[index] == "4" || pty[index] == "5" || pty[index] == "6") {
+      if (
+        pty[index] == "1" ||
+        pty[index] == "4" ||
+        pty[index] == "5" ||
+        pty[index] == "6"
+      ) {
         sky[index] = "비";
-      }
-      else if(pty[index] != "0"){
+      } else if (pty[index] != "0") {
         sky[index] = "눈";
       }
     }
@@ -194,101 +192,95 @@ app.get('/weather', function(req,res){
     var list = new Array();
 
     for (let index = 0; index < 6; index++) {
-        var testList = new Array();
-        testList.push(time[index]);
-        testList.push(sky[index]);
-        list.push(testList);
+      var testList = new Array();
+      testList.push(time[index]);
+      testList.push(sky[index]);
+      list.push(testList);
     }
     data.weather = list;
 
     var jsonData = JSON.stringify(data);
 
-    res.send(jsonData)
-  })
+    res.send(jsonData);
+  });
+});
 
+app.get("/info", function (req, res) {
+  var request = require("request");
+  var url2 =
+    "https://www.tournmaster.com/seantour_map/travel/getBeachCongestionApi.do";
 
-  
-})
-
-app.get('/info', function(req,res) { 
-  var request = require('request');
-  var url2 = 'https://www.tournmaster.com/seantour_map/travel/getBeachCongestionApi.do';
-  
   var beach_name = encodeURI(req.query.beach_name);
 
   var options = {
-    'method': 'GET',
-    'url': url2,
-    'headers': {
-    }
+    method: "GET",
+    url: url2,
+    headers: {},
   };
 
   request(options, function (error, response) {
     if (error) throw new Error(error);
 
-    var data = JSON.parse(response.body)
-    
+    var data = JSON.parse(response.body);
+
     var current_people = -1;
     var congestion = -1;
 
     for (key in data) {
       if (beach_name == encodeURI(data[key].poiNm)) {
-        current_people = data[key].uniqPop
-        congestion = data[key].congestion
+        current_people = data[key].uniqPop;
+        congestion = data[key].congestion;
         break;
       }
     }
 
-    var data = new Object() ;
-		
+    var data = new Object();
+
     data.current_people = current_people;
     data.congestion = congestion;
-  
+
     var jsonData = JSON.stringify(data);
 
-    res.send(jsonData)
+    res.send(jsonData);
   });
-})
+});
 
-    
-  app.get("/info", function (req, res) {
-    var request = require("request");
-    var url2 =
-      "https://www.tournmaster.com/seantour_map/travel/getBeachCongestionApi.do";
+app.get("/info", function (req, res) {
+  var request = require("request");
+  var url2 =
+    "https://www.tournmaster.com/seantour_map/travel/getBeachCongestionApi.do";
 
-    var beach_name = encodeURI(req.query.beach_name);
+  var beach_name = encodeURI(req.query.beach_name);
 
-    var options = {
-      method: "GET",
-      url: url2,
-      headers: {},
-    };
+  var options = {
+    method: "GET",
+    url: url2,
+    headers: {},
+  };
 
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
 
-      var data = JSON.parse(response.body);
+    var data = JSON.parse(response.body);
 
-      var current_people = -1;
-      var congestion = -1;
+    var current_people = -1;
+    var congestion = -1;
 
-      for (key in data) {
-        if (beach_name == encodeURI(data[key].poiNm)) {
-          current_people = data[key].uniqPop;
-          congestion = data[key].congestion;
-          break;
-        }
+    for (key in data) {
+      if (beach_name == encodeURI(data[key].poiNm)) {
+        current_people = data[key].uniqPop;
+        congestion = data[key].congestion;
+        break;
       }
+    }
 
-      var data = new Object();
+    var data = new Object();
 
-      data.current_people = current_people;
-      data.congestion = congestion;
+    data.current_people = current_people;
+    data.congestion = congestion;
 
-      var jsonData = JSON.stringify(data);
+    var jsonData = JSON.stringify(data);
 
-      res.send(jsonData);
-    });
-  })
-);
-
+    res.send(jsonData);
+  });
+});
--- public/app.js
+++ public/app.js
@@ -13,12 +13,8 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 // 장소 검색 객체를 생성합니다
 var ps = new kakao.maps.services.Places();
 
-// 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
-var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-
 // 키워드로 장소를 검색합니다
-searchPlaces();
-
+ps.keywordSearch("해수욕장 ", placesSearchCB);
 
 // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
 var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
@@ -28,46 +24,19 @@ searchPlaces();
 
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
-    const keyword = document.getElementById("keyword").value;
-    console.log(keyword);
-    if(keyword === ' ') {
-        ps.keywordSearch('해수욕장', placesSearchCB)
-    }
-    else {
-        ps.keywordSearch(keyword, placesSearchCB);
-    }
+  const keyword = document.getElementById("keyword").value;
+  console.log(keyword);
+  if (keyword === " ") {
+    ps.keywordSearch("해수욕장", placesSearchCB);
+  } else {
+    ps.keywordSearch(keyword, placesSearchCB);
+  }
 
   // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
 }
 
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-function placesSearchCB (data, status) {
-    if (status === kakao.maps.services.Status.OK) {
-
-
-// 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
-function placesSearchCB(data, status, pagination) {
-  if (status === kakao.maps.services.Status.OK) {
-    // 정상적으로 검색이 완료됐으면
-    // 검색 목록과 마커를 표출합니다
-    displayPlaces(data);
-
-    // 페이지 번호를 표출합니다
-    displayPagination(pagination);
-  } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-    alert("검색 결과가 존재하지 않습니다.");
-    return;
-  } else if (status === kakao.maps.services.Status.ERROR) {
-    alert("검색 결과 중 오류가 발생했습니다.");
-    return;
-  }
-}
-
-// 키워드로 장소를 검색합니다
-ps.keywordSearch("해수욕장 ", placesSearchCB);
-
-// 키워드 검색 완료 시 호출되는 콜백함수 입니다
-function placesSearchCB(data, status, pagination) {
+function placesSearchCB(data, status) {
   if (status === kakao.maps.services.Status.OK) {
     // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
     // LatLngBounds 객체에 좌표를 추가합니다
@@ -85,12 +54,18 @@ function placesSearchCB(data, status, pagination) {
 
 // 지도에 마커를 표시하는 함수입니다
 function displayMarker(place) {
-
   // 마커를 생성하고 지도에 표시합니다
   var marker = new kakao.maps.Marker({
     map: map,
     position: new kakao.maps.LatLng(place.y, place.x),
   });
+
+  var markerImage = new kakao.maps.MarkerImage(
+    "img/marker.png",
+    new kakao.maps.Size(25, 25),
+    new kakao.maps.Point(13, 34)
+  );
+  marker.setImage(markerImage);
 
   // 마커에 클릭이벤트를 등록합니다
   kakao.maps.event.addListener(marker, "click", function () {
@@ -98,111 +73,64 @@ function displayMarker(place) {
     infowindow.setContent(
       '<div style="padding:5px;font-size:12px;">' + place.place_name + "</div>"
     );
+    console.log(place);
     infowindow.open(map, marker);
     const placeName = place.place_name;
-    const index = placeName.indexOf("해");
+    const index = placeName.indexOf("해", 1);
     let name = placeName.slice(0, index);
-    if (placeName === "해운대해수욕장") {
-      name = "해운대";
+    const deletePeople = document.querySelector("span");
+    if (deletePeople !== null) {
+      deletePeople.remove();
     }
-    getRes(name).then((res) => {
+    const deleteWeather = document.querySelector("ul");
+    if (deleteWeather !== null) {
+      deleteWeather.remove();
+    }
+    const deleteUrl = document.querySelector("a");
+    if (deleteUrl !== null) {
+      deleteUrl.remove();
+    }
+
+    const url = place.place_url;
+    const a = document.createElement("a");
+    a.append(url);
+    a.href = url;
+    a.target = "blank";
+    document.body.append(a);
+
+    getPeople(name).then((res) => {
       const people = res.data.current_people;
-      const deleting = document.querySelector("span");
-      if (deleting !== null) {
-        deleting.remove();
-      }
+
       const span = document.createElement("span");
       if (people === -1) {
-        span.append("정보 없음");
+        span.append("사람 수 정보 없음");
       } else {
         span.append(`현재 사람수: ${res.data.current_people}`);
       }
       document.body.append(span);
     });
+
+    // console.log(marker.getPosition());
+    const lat = parseFloat(place.x);
+    const lon = parseFloat(place.y);
+    getWether(lon, lat).then((res) => {
+      const datas = res.data.weather;
+      // console.log(datas);
+      const ul = document.createElement("ul");
+      datas.forEach((data) => {
+        const li = document.createElement("li");
+        li.append(data);
+        ul.append(li);
+        document.body.append(ul);
+      });
+    });
   });
 }
 
-    var markerImage = new kakao.maps.MarkerImage(
-        'img/marker.png',
-        new kakao.maps.Size(25, 25), new kakao.maps.Point(13, 34));
-    marker.setImage(markerImage);
-    
-
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-        console.log(place);
-        infowindow.open(map, marker);
-        const placeName = place.place_name;
-        const index = placeName.indexOf('해', 1);
-        let name = placeName.slice(0, index);
-        const deletePeople = document.querySelector('span');
-        if(deletePeople !== null) {
-            deletePeople.remove();
-        }
-        const deleteWeather = document.querySelector('ul');
-        if(deleteWeather !== null) {
-            deleteWeather.remove();
-        }
-        const deleteUrl = document.querySelector('a');
-        if(deleteUrl !== null) {
-            deleteUrl.remove();
-        }
-
-        const url = place.place_url;
-        const a = document.createElement('a');
-        a.append(url);
-        a.href=url;
-        a.target="blank";
-        document.body.append(a);
-
-        getPeople(name)
-        .then(res => {
-            const people = res.data.current_people;
-
-            const span = document.createElement('span');
-            if(people === -1) {
-                span.append('사람 수 정보 없음')
-            }
-            else {
-            span.append(`현재 사람수: ${res.data.current_people}`);
-            }
-            document.body.append(span);
-        })
-
-        // console.log(marker.getPosition());
-        const lat = parseFloat(place.x)
-        const lon = parseFloat(place.y)
-        getWether(lon, lat)
-        .then(res => {
-            const datas = res.data.weather;
-            // console.log(datas);
-            const ul = document.createElement('ul');
-            datas.forEach(data => {
-                const li = document.createElement('li');
-                li.append(data);
-                ul.append(li);
-                document.body.append(ul);
-            })
-        })
-    });
-
-}
-
-
-
-
-
-
-
 async function getPeople(loc) {
-    return await axios.get(`http://localhost:3000/info?beach_name=${loc}`)
-
+  return await axios.get(`http://localhost:3000/info?beach_name=${loc}`);
 }
 
 async function getWether(lat, lon) {
-    return await axios.get(`http://localhost:3000/weather?lat=${lat}&lng=${lon}`)
+  return await axios.get(`http://localhost:3000/weather?lat=${lat}&lng=${lon}`);
 }
-
-
